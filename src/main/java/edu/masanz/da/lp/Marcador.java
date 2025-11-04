471d4714d67a54c2773c6b38a5569a6b74a7c004
Sara Vertiz <svertizcon1@educacion.navarra.es>
2025-11-04 12:57:21 +0100

    TODOs 61...69

--- src/main/java/edu/masanz/da/lp/Marcador.java
+++ src/main/java/edu/masanz/da/lp/Marcador.java
@@ -14,8 +14,10 @@ public class Marcador {
      * Constructor vacío. Inicializa ambos valores a 0.
      */
     public Marcador() {
-        // TODO 61: inicializa ambos valores a 0.
+        // HECHO TODO 61: inicializa ambos valores a 0.
         // Puedes utilizar this(...) para llamar al otro constructor.
+        this.v1 = 0;
+        this.v2 = 0;
 
     }
 
@@ -25,8 +27,9 @@ public class Marcador {
      * @param v2 Valor para el segundo equipo.
      */
     public Marcador(int v1, int v2) {
-        // TODO 62: inicializa los atributos con los parámetros proporcionados.
-
+        // HECHO TODO 62: inicializa los atributos con los parámetros proporcionados.
+        this.v1 = v1;
+        this.v2 = v2;
 
     }
 
@@ -37,8 +40,8 @@ public class Marcador {
      * @return Valor del primer equipo.
      */
     public int getV1() {
-        // TODO 63: devuelve el valor del primer equipo.
-        return 0;
+        // HECHO TODO 63: devuelve el valor del primer equipo.
+        return v1;
     }
 
     /**
@@ -46,7 +49,8 @@ public class Marcador {
      * @param v1 Valor del primer equipo.
      */
     public void setV1(int v1) {
-        // TODO 64: establece el valor del primer equipo.
+        // HECHO TODO 64: establece el valor del primer equipo.
+        this.v1 = v1;
 
     }
 
@@ -55,8 +59,8 @@ public class Marcador {
      * @return Valor del segundo equipo.
      */
     public int getV2() {
-        // TODO 65: devuelve el valor del segundo equipo.
-        return 0;
+        // HECHO TODO 65: devuelve el valor del segundo equipo.
+        return v2;
     }
 
     /**
@@ -64,8 +68,8 @@ public class Marcador {
      * @param v2 Valor del segundo equipo.
      */
     public void setV2(int v2) {
-        // TODO 66: establece el valor del segundo equipo.
-
+        // HECHO TODO 66: establece el valor del segundo equipo.
+        this.v2 = v2;
     }
 
     // endregion
@@ -76,8 +80,9 @@ public class Marcador {
      * @param v2 Valor del segundo equipo.
      */
     public void setV1V2(int v1, int v2) {
-        // TODO 67: establece ambos valores del marcador.
-
+        // HECHO TODO 67: establece ambos valores del marcador.
+            this.v1 = v1;
+            this.v2 = v2;
 
     }
 
@@ -86,14 +91,14 @@ public class Marcador {
      * @return 1 si gana el primer equipo, 2 si gana el segundo, 0 si hay empate.
      */
     public int getGanador() {
-        // TODO 68: devuelve el número del equipo ganador.
-
-
-
-
-
-
-        return 0;
+        // HECHO TODO 68: devuelve el número del equipo ganador.
+        if (v1 > v2){
+            return 1;
+        } else if (v2 > v1){
+            return 2;
+        } else {
+            return 0;
+        }
     }
 
     /**
@@ -102,8 +107,8 @@ public class Marcador {
      */
     @Override
     public String toString() {
-        // TODO 69: devuelve una representación textual del marcador.
-        return "0-0";
+        //HECHO TODO 69: devuelve una representación textual del marcador.
+        return "["+v1+"-"+v2+"]";
     }
 
 }
